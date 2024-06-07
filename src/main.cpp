316bc5c126a7d37e3b0098ab360ae1645d720269
hldh214 <hldh214@gmail.com>
2024-06-07 17:03:12 +0900

    Refactor code to use FreeRTOS tasks instead of ArduinoThread
    Refactored main.cpp to use FreeRTOS tasks for threading instead of the previous ArduinoThread library. Removed unnecessary comments and code, and updated platformio.ini to add a build flag enabling USB CDC on boot.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,111 +1,71 @@
-#include <esp32-hal-gpio.h>
 #include <Arduino.h>
-#include "USB.h"
-#include "USBHIDMouse.h"
-#include "USBHIDKeyboard.h"
-#include "ThreadController.h"
-//#include "Adafruit_NeoPixel.h"
+#include <USBHIDMouse.h>
+#include <USBHIDKeyboard.h>
+#include <USB.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 USBHIDMouse Mouse;
 USBHIDKeyboard Keyboard;
 
-ThreadController tc = ThreadController();
+TaskHandle_t mouseTaskHandle;
+TaskHandle_t buyTaskHandle;
+TaskHandle_t fireTaskHandle;
+TaskHandle_t moveTaskHandle;
 
-//int bootPin = 0;
-//bool isDisabled = false;
+[[noreturn]] void mouseTask(__attribute__((unused)) void *pvParameters) {
+    while (true) {
+        Mouse.move(0, -1);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        Mouse.move(0, 1);
 
-//Adafruit_NeoPixel pixels(1, 48, NEO_GRB + NEO_KHZ800);
-
-void mouse() {
-    // move mouse up
-    Mouse.move(0, -1);
-    delay(5);
-    // move mouse down
-    Mouse.move(0, 1);
+        vTaskDelay(pdMS_TO_TICKS(8000));
+    }
 }
 
-void buy_weapon() {
-    // press 'b' key
-    Keyboard.write('b');
-    // press 'x' key for melee weapon
-    Keyboard.write('x');
-    // press '3' key for melee weapon
-    Keyboard.write('3');
+[[noreturn]] void buyTask(__attribute__((unused)) void *pvParameters) {
+    while (true) {
+        // press 'b' key
+        Keyboard.write('b');
+        // press 'f' key for melee weapon
+        Keyboard.write('f');
+        // press '3' key for melee weapon
+        Keyboard.write('3');
+
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
 }
 
-void fire() {
-    // press 'p' key
-    Keyboard.write('p');
+[[noreturn]] void fireTask(__attribute__((unused)) void *pvParameters) {
+    while (true) {
+        // press 'p' key
+        Keyboard.write('p');
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
 }
 
-void move() {
-    // press 'w' key
-    Keyboard.write('w');
+[[noreturn]] void moveTask(__attribute__((unused)) void *pvParameters) {
+    while (true) {
+        // press 'w' key
+        Keyboard.write('w');
+        // press 'a' key
+        Keyboard.write('a');
 
-    // press 'a' key
-    Keyboard.write('a');
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
 }
 
-//void stop_rgb() {
-//    pixels.setPixelColor(0, Adafruit_NeoPixel::Color(0, 0, 0));
-//}
-//
-//void start_rgb() {
-//    for (int r = 0; r < 255; r += 10) {
-//        for (int g = 0; g < 255; g += 10) {
-//            for (int b = 0; b < 255; b += 10) {
-//                pixels.setPixelColor(0, Adafruit_NeoPixel::Color(r, g, b));
-//                delay(10);
-//            }
-//        }
-//    }
-//}
-
-void setup() {
-//    pinMode(bootPin, INPUT_PULLUP);
-//    pixels.begin();
-
+__attribute__((unused)) void setup() {
     Mouse.begin();
     Keyboard.begin();
     USB.begin();
 
-    auto *mouseThread = new Thread();
-    mouseThread->onRun(mouse);
-    mouseThread->setInterval(8000);
-
-    auto *buyWeaponThread = new Thread();
-    buyWeaponThread->onRun(buy_weapon);
-    buyWeaponThread->setInterval(5000);
-
-    auto *fireThread = new Thread();
-    fireThread->onRun(fire);
-    fireThread->setInterval(500);
-
-    auto *moveThread = new Thread();
-    moveThread->onRun(move);
-    moveThread->setInterval(2000);
-
-//    auto *rgbThread = new Thread();
-//    rgbThread->onRun(start_rgb);
-//    rgbThread->setInterval(10);
-
-    tc.add(mouseThread);
-    tc.add(buyWeaponThread);
-    tc.add(fireThread);
-    tc.add(moveThread);
-//    tc.add(rgbThread);
+    xTaskCreate(mouseTask, "Mouse Task", 2048, nullptr, 1, &mouseTaskHandle);
+    xTaskCreate(buyTask, "Buy Task", 2048, nullptr, 1, &buyTaskHandle);
+    xTaskCreate(fireTask, "Fire Task", 2048, nullptr, 1, &fireTaskHandle);
+    xTaskCreate(moveTask, "Move Task", 2048, nullptr, 1, &moveTaskHandle);
 }
 
 void loop() {
-    tc.run();
-
-//    if (digitalRead(bootPin) == LOW) {
-//        isDisabled = !isDisabled;
-//    }
-//
-//    if (!isDisabled) {
-//        tc.run();
-//    } else {
-//        stop_rgb();
-//    }
 }
