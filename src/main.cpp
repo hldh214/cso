65d06031d28fa65b63607f1385fa25657d1c6eec
hldh214 <hldh214@gmail.com>
2024-06-13 21:43:34 +0900

    Switched platform to atmelavr and upgraded task functionality
    The platform has been switched from espressif32 to atmelavr in platformio.ini file. The main.cpp file has been significantly updated, improving task functionality by introducing new actions such as mouse clicks and key presses. Some previous actions have been commented out and task resource allocations have been redefined. The specific board used has also been changed to leonardo.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,41 @@
 #include <Arduino.h>
-#include <USBHIDMouse.h>
-#include <USBHIDKeyboard.h>
-#include <USB.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include <Bounce2.h>
+//#include <Bounce2.h>
+#include <AbsMouse.h>
+#include <Keyboard.h>
+#include <Arduino_FreeRTOS.h>
 
 
-#define LED_PIN 48
-#define BOOT_PIN 0
+//#define LED_PIN 48
+//#define BOOT_PIN 0
 
-Bounce2::Button bootButton = Bounce2::Button(); // INSTANTIATE A Bounce2::Button OBJECT
-
-USBHIDMouse Mouse;
-USBHIDKeyboard Keyboard;
+//Bounce2::Button bootButton = Bounce2::Button(); // INSTANTIATE A Bounce2::Button OBJECT
 
 TaskHandle_t mouseTaskHandle;
 TaskHandle_t buyTaskHandle;
 TaskHandle_t F2AndReviveTaskHandle;
 TaskHandle_t moveAndFireTaskHandle;
 
-bool running = false;
+bool running = true;
+
+void mouseClick(uint8_t button = MOUSE_LEFT) {
+    AbsMouse.press(button);
+    vTaskDelay(pdMS_TO_TICKS(50));
+    AbsMouse.release(button);
+}
 
 [[noreturn]] void mouseTask(__attribute__((unused)) void *pvParameters) {
     while (true) {
-        Mouse.move(0, -10);
-        vTaskDelay(pdMS_TO_TICKS(10));
-        Mouse.move(0, 10);
+        // waiting room -> game start
+        AbsMouse.move(2750, 1405);
+        mouseClick();
 
-        vTaskDelay(pdMS_TO_TICKS(8000));
+        vTaskDelay(pdMS_TO_TICKS(4000));
+
+        // quit score board -> return to the waiting room
+        AbsMouse.move(2750, 1730);
+        mouseClick();
+
+        vTaskDelay(pdMS_TO_TICKS(4000));
     }
 }
 
@@ -39,6 +46,12 @@ bool running = false;
         // press 's' key for melee weapon
         Keyboard.write('s');
         vTaskDelay(pdMS_TO_TICKS(50));
+        // press 'b' key
+        Keyboard.write('b');
+        vTaskDelay(pdMS_TO_TICKS(50));
+        // press 's' key for secondary weapon
+        Keyboard.write('f');
+        vTaskDelay(pdMS_TO_TICKS(50));
         // press '3' key for melee weapon
         Keyboard.write('3');
 
@@ -46,6 +59,9 @@ bool running = false;
         vTaskDelay(pdMS_TO_TICKS(50));
         // press 'ESC' key for close the popup window
         Keyboard.write(KEY_ESC);
+
+        // press 'r' key for revive
+        Keyboard.write('r');
 
         vTaskDelay(pdMS_TO_TICKS(5000));
     }
@@ -55,11 +71,8 @@ bool running = false;
     while (true) {
         // press 'f2' key for auto buying
         Keyboard.write(KEY_F2);
-        vTaskDelay(pdMS_TO_TICKS(50));
-        // press 'r' key for revive
-        Keyboard.write('r');
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
@@ -80,50 +93,51 @@ bool running = false;
 }
 
 __attribute__((unused)) void setup() {
-    Mouse.begin();
     Keyboard.begin();
-    USB.begin();
+    AbsMouse.init(3840, 2160);
 
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(BOOT_PIN, INPUT_PULLUP);
-    digitalWrite(LED_PIN, LOW);
+//    pinMode(LED_PIN, OUTPUT);
+//    pinMode(BOOT_PIN, INPUT_PULLUP);
+//    digitalWrite(LED_PIN, LOW);
 
-    bootButton.attach(BOOT_PIN, INPUT_PULLUP);
-    bootButton.interval(5);
-    bootButton.setPressedState(LOW);
+//    bootButton.attach(BOOT_PIN, INPUT_PULLUP);
+//    bootButton.interval(5);
+//    bootButton.setPressedState(LOW);
 
-    xTaskCreate(mouseTask, "Mouse Task", 2048, nullptr, 1, &mouseTaskHandle);
-    xTaskCreate(buyTask, "Buy Task", 2048, nullptr, 1, &buyTaskHandle);
-    xTaskCreate(F2AndReviveTask, "F2 And Revive Task", 2048, nullptr, 1, &F2AndReviveTaskHandle);
-    xTaskCreate(moveAndFireTask, "Move And Fire Task", 2048, nullptr, 1, &moveAndFireTaskHandle);
+    xTaskCreate(mouseTask, "Mouse Task", 128, nullptr, 1, &mouseTaskHandle);
+    xTaskCreate(buyTask, "Buy Task", 128, nullptr, 1, &buyTaskHandle);
+    xTaskCreate(F2AndReviveTask, "F2 And Revive Task", 128, nullptr, 1, &F2AndReviveTaskHandle);
+    xTaskCreate(moveAndFireTask, "Move And Fire Task", 128, nullptr, 1, &moveAndFireTaskHandle);
 
-    // fixme: this is so ugly...
-    vTaskSuspend(mouseTaskHandle);
-    vTaskSuspend(buyTaskHandle);
-    vTaskSuspend(F2AndReviveTaskHandle);
-    vTaskSuspend(moveAndFireTaskHandle);
+//    // fixme: this is so ugly...
+//    vTaskSuspend(mouseTaskHandle);
+//    vTaskSuspend(buyTaskHandle);
+//    vTaskSuspend(F2AndReviveTaskHandle);
+//    vTaskSuspend(moveAndFireTaskHandle);
+
+    vTaskStartScheduler();
 }
 
 void loop() {
     // UPDATE THE BUTTON BY CALLING .update() AT THE BEGINNING OF THE LOOP:
-    bootButton.update();
+//    bootButton.update();
+//
+//    if (bootButton.pressed()) {
+//        running = !running;
+//    }
 
-    if (bootButton.pressed()) {
-        running = !running;
-    }
-
-    if (running) {
-        digitalWrite(LED_PIN, HIGH);
-        vTaskResume(mouseTaskHandle);
-        vTaskResume(buyTaskHandle);
-        vTaskResume(F2AndReviveTaskHandle);
-        vTaskResume(moveAndFireTaskHandle);
-    } else {
-        Keyboard.releaseAll();
-        digitalWrite(LED_PIN, LOW);
-        vTaskSuspend(mouseTaskHandle);
-        vTaskSuspend(buyTaskHandle);
-        vTaskSuspend(F2AndReviveTaskHandle);
-        vTaskSuspend(moveAndFireTaskHandle);
-    }
+//    if (running) {
+////        digitalWrite(LED_PIN, HIGH);
+//        vTaskResume(mouseTaskHandle);
+////        vTaskResume(buyTaskHandle);
+////        vTaskResume(F2AndReviveTaskHandle);
+////        vTaskResume(moveAndFireTaskHandle);
+//    } else {
+//        Keyboard.releaseAll();
+////        digitalWrite(LED_PIN, LOW);
+//        vTaskSuspend(mouseTaskHandle);
+//        vTaskSuspend(buyTaskHandle);
+//        vTaskSuspend(F2AndReviveTaskHandle);
+//        vTaskSuspend(moveAndFireTaskHandle);
+//    }
 }
